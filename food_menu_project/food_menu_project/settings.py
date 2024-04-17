f8ad721972704cfedbb21fb530e3a6b652420dbd
sagar-bhukya <sagarbhukya08@gmail.com>
2024-04-17 16:31:26 +0530

    Initial commit of Django project

--- food_menu_project/food_menu_project/settings.py
+++ food_menu_project/food_menu_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-opz$t+1a+++rcd0*v(3reur1!h=u8ff1e-l=@@=r*=(1u=p(&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
