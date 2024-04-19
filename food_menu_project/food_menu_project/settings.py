7842b4c7c41e40d78e705ca41675c9ee07aec729
sagar-bhukya <sagarbhukya08@gmail.com>
2024-04-19 11:20:24 +0530

    push

--- food_menu_project/food_menu_project/settings.py
+++ food_menu_project/food_menu_project/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'food_menu_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
